f2f09b28ebbd13d0d055a86b51cc1be633d76066
reedm26 <reedm26@yahoo.com>
2019-12-20 07:13:06 -0700

    added an action for my bugs

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+// @ts-ignore
+import axios from "axios";
 
-Vue.use(Vuex)
+let _sandBox = axios.create({
+  baseURL: "//localhost:3000/api",
+  timeout: 3000
+});
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    bugs: []
   },
   mutations: {
+    setAllBugs(state, data) {
+      state.bugs = data;
+    }
   },
   actions: {
+    async getBugs({ commit, dispatch }) {
+      let res = await _sandBox.get("bugs");
+      commit("setAllBugs", res.data);
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
